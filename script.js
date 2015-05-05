15e15095a1d06a3a4ff2c5d61dfa50d6d60323ff
Ricardo Oliveira <oliveira.ricardo@hotmail.com>
2015-05-05 15:04:51 +0000

    add base map

---
+++ script.js
@@ -0,0 +1,13 @@
+var map = L.map('map', {
+         'zoomControl': false,
+     }).setView([39.739800, -104.989276], 13);
+
+     //zoom custom position
+     L.control.zoom({
+         position: 'topright'
+     }).addTo(map);
+
+     //the base map
+     L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
+         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+     }).addTo(map);
