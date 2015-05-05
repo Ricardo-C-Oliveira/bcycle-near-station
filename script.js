0dab7ae47cf399424ff60849725fed0d138402ea
Ricardo Oliveira <oliveira.ricardo@hotmail.com>
2015-05-05 16:19:23 +0000

    add stations data

--- script.js
+++ script.js
@@ -11,3 +11,23 @@ var map = L.map('map', {
      L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png', {
          attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
      }).addTo(map);
+
+var data = L.geoJson(stations, {
+pointToLayer: function (feature, latlgn) {
+    var popupContent = "<b>Station Name: </b>" + feature.properties.STATION;
+    return L.circleMarker(latlgn, Style(feature)).bindPopup(popupContent);
+},
+});
+
+//function that defines style for each point
+     function Style(feature) {
+         return {
+             fillColor: white,
+             stroke: false,
+             opacity: 1,
+             color: "#000",
+             fillOpacity: 1,
+             weight: 1,
+             radius: 5
+         };
+     }
