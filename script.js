7e5ef6a027374bc5513d95a1e72e25d6d3651338
Ricardo-C-Oliveira <oliveira.ricardo@hotmail.com>
2015-05-06 14:16:00 -0600

    core is working

--- script.js
+++ script.js
@@ -1,43 +1,76 @@
 var map = L.map('map', {
-         'zoomControl': false,
-     }).setView([39.739800, -104.989276], 13);
+  'zoomControl': false,
+}).setView([39.739800, -104.989276], 14);
 
-     //zoom custom position
-     L.control.zoom({
-         position: 'topright'
-     }).addTo(map);
+//zoom custom position
+L.control.zoom({
+  position: 'topright'
+}).addTo(map);
 
-     //the base map
-     L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png', {
-         attribution: 'Map tiles by <a href="http://cartodb.com/attributions#basemaps">CartoDB</a>, under <a href="https://creativecommons.org/licenses/by/3.0/">CC BY 3.0</a>. Data by <a href="http://www.openstreetmap.org/">OpenStreetMap</a>, under ODbL.'
-     }).addTo(map);
+//the base map
+L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png', {
+  attribution: 'Map tiles by <a href="http://cartodb.com/attributions#basemaps">CartoDB</a>, under <a href="https://creativecommons.org/licenses/by/3.0/">CC BY 3.0</a>. Data by <a href="http://www.openstreetmap.org/">OpenStreetMap</a>, under ODbL.'
+}).addTo(map);
 
 //stations data
 var data = L.geoJson(stations, {
-pointToLayer: function (feature, latlgn) {
+  pointToLayer: function(feature, latlgn) {
     var popupContent = '<p style="font-family: Roboto, sans-serif; color: white; font-size: 16px"><b>Station Name: </b><span style="color: red"><b>' + feature.properties.STATION + '</b></span></p>';
     return L.circleMarker(latlgn, Style(feature)).bindPopup(popupContent);
-},
+  },
 }).addTo(map);
 
 //function that defines style for each point
-     function Style(feature) {
-         return {
-             fillColor: "#1015a5",
-             stroke: 1,
-             opacity: 1,
-             color: "#dd0d24",
-             fillOpacity: 1,
-             weight: 1,
-             radius: 6
-         };
-     }
+function Style(feature) {
+  return {
+    fillColor: "#1015a5",
+    stroke: 1,
+    opacity: 1,
+    color: "#dd0d24",
+    fillOpacity: 1,
+    weight: 1,
+    radius: 6
+  };
+}
 
-     var marker = L.marker(new L.LatLng(39.739800, -104.989276), {
-        icon: L.AwesomeMarkers.icon({
-             icon: 'fa-bicycle',
-             markerColor: 'red',
-             prefix: 'fa'
-         }),
-         draggable: true
-     }).addTo(map);
+var marker = L.marker(new L.LatLng(39.736686, -105.002213), {
+  icon: L.AwesomeMarkers.icon({
+    icon: 'fa-bicycle',
+    markerColor: 'red',
+    prefix: 'fa'
+  }),
+  draggable: true
+}).addTo(map);
+
+
+
+
+//remove nearest station
+function removeNear() {
+  map.removeLayer(near);
+}
+
+//custom near icon
+var customIcon  = L.AwesomeMarkers.icon({
+  icon: 'fa-bicycle',
+  markerColor: 'blue',
+  prefix: 'fa'
+});
+
+//add nearest station
+function addNear() {
+  //make marker a geoJson
+  var pointMarker = marker.toGeoJSON();
+  //turf nearest
+  nearest = turf.nearest(pointMarker, stations);
+  near = L.geoJson(nearest, {
+    pointToLayer: function (feature, latlng) {
+      return L.marker(latlng, {icon: customIcon});
+    }
+  }).addTo(map);
+}
+
+marker.on('drag', function(){
+  removeNear(), addNear()
+});
+addNear();
